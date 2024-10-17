c02e2e2f18c46dbc823673a673236a0ee71b3302
EmilAsk-Dev <carl.emil.ask1@gmail.com>
2024-10-17 12:10:20 +0200

    Add project files.

---
+++ User/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitTrack.User
+{
+    abstract class Person
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public Person(string Username, string Password)
+        {
+            this.UserName = Username;
+            this.Password = Password;
+        }
+
+        public abstract void SignIn(string username, string password);
+
+    }
+}
---
+++ User/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitTrack.User
+{
+    class User : Person
+    {
+        public User(string Username, string Password) : base(Username, Password) 
+        {
+            this.UserName = Username;
+            this.Password = Password;
+        }
+
+
+        public override void SignIn(string username, string password)
+        {
+            
+        }
+    }
+}
